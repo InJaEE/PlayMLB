9d1567a73e44fff1265b057bccdca4c9a99097e0
InJaEE <in11202@naver.com>
2020-05-21 00:17:17 +0900

    fix production server url

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const serverURL = () => {
-	if (process.env.NODE_ENV) {
-		return 'https://playmlb-server.herokuapp.com/';
-	} else {
-		return 'http://localhost:3000/';
-	}
-};
+let serverURL;
+
+if (process.env.NODE_ENV === 'production') {
+	serverURL = 'https://playmlb-server.herokuapp.com/';
+} else {
+	serverURL = 'http://127.0.0.1:3000';
+}
 
 const instance = axios.create({
 	baseURL: 'https://lookup-service-prod.mlb.com/json/',
