d826531e4954f24910b2d89eb35acc6d98d14638
InJaEE <in11202@naver.com>
2020-07-01 18:23:57 +0900

    fix clock

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,13 +6,24 @@
 export default {
 	data() {
 		return {
-			time: new Date(),
+			time: '',
 		};
 	},
 	methods: {
 		now() {
 			return setInterval(() => {
 				this.time = new Date();
+				const year = new Date().getFullYear();
+				let month = new Date().getMonth() + 1;
+				if (month < 10) month = '0' + month;
+				let day = new Date().getDate();
+				if (day < 10) day = '0' + day;
+				const hour = new Date().getHours();
+				let minute = new Date().getMinutes();
+				if (minute < 10) minute = '0' + minute;
+				let second = new Date().getSeconds();
+				if (second < 10) second = '0' + second;
+				this.time = `${year}.${month}.${day} ${hour}:${minute}:${second}`;
 			}, 1000);
 		},
 	},
